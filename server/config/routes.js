c99d9cf26106ae15ec45993d55eb9df9245a76b5
Derek Liu <liu.derekt@gmail.com>
2016-09-13 16:52:14 -0700

    Remove parameters on function call to articleKeywords.

--- server/config/routes.js
+++ server/config/routes.js
@@ -59,7 +59,7 @@ module.exports = function(app, express) {
 
   app.route('/test')
     .get(function(req, res) {
-      aylien.articleKeywords(req.query.input, res);
+      aylien.articleKeywords();
     });
 
   /************************ GOOGLE TRENDS **********************************/
