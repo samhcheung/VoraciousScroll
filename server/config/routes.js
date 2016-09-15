31e35e23d47f245c9c205390a08ab48b87590bb1
Derek Liu <liu.derekt@gmail.com>
2016-09-14 20:32:45 -0700

    (format) fix spacing issue for styling.

--- server/config/routes.js
+++ server/config/routes.js
@@ -27,7 +27,7 @@ module.exports = function(app, express) {
   app.use(passport.initialize());
   app.use(passport.session());
 
-  app.get('/enter', function(req, res){
+  app.get('/enter', function(req, res) {
     console.log(__dirname);
     res.sendFile(path.join(__dirname + '/../../public/layout.html'));
   });
@@ -94,7 +94,7 @@ module.exports = function(app, express) {
 
   app.route('/unsaveArticle/:id')
     .delete(function(req, res) {
-      db.unsaveArticle.delete(req, function(err, success){
+      db.unsaveArticle.delete(req, function(err, success) {
         res.send(success);
       });
     });
