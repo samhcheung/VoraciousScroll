e40ffc861c631eec046325a5e43215a9907c1f4f
Brian Kilrain <bkilrain@gmail.com>
2016-09-15 21:02:39 -0700

    turn off worker in config.js

--- server/db/config.js
+++ server/db/config.js
@@ -3,9 +3,9 @@ var prefetchWorker = require('../news-apis/workers.js');
 
 mongoose.connect('mongodb://localhost/voraciousscroll');
 
-setInterval(function() {
-    console.log('<<<<<<<<<<<<<<<<<<<<worker!>>>>>>>>>>>>>>>>>>>')
-    prefetchWorker();
-  }, 30000);
+// setInterval(function() {
+//     console.log('<<<<<<<<<<<<<<<<<<<<worker!>>>>>>>>>>>>>>>>>>>')
+//     prefetchWorker();
+//   }, 30000);
 
 module.exports = mongoose;
