e5ad5c08e8b14898a60297c2872b02a3a7f50492
Brian Kilrain <bkilrain@gmail.com>
2016-09-17 14:27:43 -0700

    clean front end code

--- public/features/nav/nav.js
+++ public/features/nav/nav.js
@@ -16,23 +16,4 @@ angular.module('smartNews')
     $location.url('/');
   };
 
-  // in case we decide to use a button and ng-click to log in
-  // $scope.clickLogin = function() {
-  //   $http({
-  //     method: 'GET',
-  //     url: '/auth/facebook'
-  //   })
-  //   .then(
-  //     function(data){
-  //       console.log('routing to /auth/facebook', data);
-  //     },
-  //     function(err){
-  //       console.log('error routing to /auth/facebook', err);
-  //     }
-  //   );
-  // };
-
-  // isAuth should be a function that returns a boolean based on whether user is authenticated or not
-  // $scope.isAuth = false;
-
 });
