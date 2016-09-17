1e08dac777c508df7313bf3e5a2e09f471c92030
Brian Kilrain <bkilrain@gmail.com>
2016-09-17 13:40:52 -0700

    Set production port to 80

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 var app = require('./server.js');
-var port = 3000;
+var port = process.env.NODE_ENV === 'production' ? 80 : 3000;
 
 app.listen(port, function() {
   console.log('SmartNews server listening on port 3000.');
